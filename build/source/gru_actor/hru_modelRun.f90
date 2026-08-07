f1c4d871b98be9fe945d4d2843bace9ec030edd2
seantrim <strim201325@gmail.com>
2026-08-07 04:34:02 -0600

    Added public subroutine for Newton iteration convergence stats initialization in hru_modelRun.f90.

--- build/source/gru_actor/hru_modelRun.f90
+++ build/source/gru_actor/hru_modelRun.f90
@@ -67,6 +67,7 @@ USE mDecisions_module,only:&               ! look-up values for LAI decisions
 implicit none
 private
 public::runPhysics
+public::initialize_convStruct
 contains
 
 ! Runs the model physics for an HRU
@@ -342,4 +343,19 @@ end subroutine runPhysics
     ! If the global default tolerance flag is set, then override the specific tolerances 
   end subroutine set_sundials_tolerances
 #endif
+
+  subroutine initialize_convStruct(hru_data)
+    ! *** initialize Newton iteration convergence stat variables for nested Newton and homegrown solvers ***
+    implicit none
+    type(hru_type), intent(inout)             :: hru_data               ! c_ptr to -- hru data
+
+    hru_data % convStruct % high_level_step_reductions        = 0_i4b
+    hru_data % convStruct % low_level_step_reductions         = 0_i4b
+    hru_data % convStruct % low_level_step_reductions_coupled = 0_i4b  
+    hru_data % convStruct % splitting_failures                = 0_i4b
+    hru_data % convStruct % splitting_failures_coupled        = 0_i4b
+    hru_data % convStruct % classical_steps_coupled           = 0_i4b 
+    hru_data % convStruct % nested_steps_coupled              = 0_i4b
+  end subroutine initialize_convStruct
+
 end module summa_modelRun
